26fccd1e7bf1f9fb2ed11eb07a48fb76be83ff73
BlixDyrseth <71275233+BlixDyrseth@users.noreply.github.com>
2021-05-14 20:10:51 +0200

    updated blogs js and css

--- js/blogs.js
+++ js/blogs.js
@@ -1,4 +1,4 @@
-const url = "https://ingridblixdyrseth.no/blog/wp-json/wp/v2/posts?_embed";
+const url = "https://ingridblixdyrseth.no/blog/wp-json/wp/v2/posts?per_page=12&?_embed";
 
 const blogContainer = document.querySelector(".blog-container");
 
@@ -7,21 +7,23 @@ async function getPosts() {
         const response = await fetch(url);
         const posts = await response.json();
 
-        console.log(posts);
 
         blogContainer.innerHTML = "";
 
-    
 
-    posts.forEach(function(post) {
-        blogContainer.innerHTML += `<a href="post.html?id=${post.id}">
-        <div class="post-block">
-        <h2>${post.title.rendered}</h2>
-        </a>
-        <img class="featured-pic" src=${post.featured_media}"/>
-        <p>${post.excerpt.rendered}</p>
-        <a href="post.html?id=${post.id}"><div class="button">Read more</div></a>`
-    });
+        for(let i = 0; i < posts.length; i++) {
+   
+            console.log(posts[i]);
+            const post = posts[i];
+
+            blogContainer.innerHTML += `<a href="post.html?id=${post.id}">
+            <div class="post-block">
+            <h2>${post.title.rendered}</h2>
+            </a>
+            <img class="featured-pic" src=${post.featured_media}"/>
+            <p>${post.excerpt.rendered}</p>
+            <a href="post.html?id=${post.id}"><div class="button">Read more</div></a>`
+        } 
 
    }
     catch(error) {
